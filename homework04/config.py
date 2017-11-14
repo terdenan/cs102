691a83a842c2373d7115a849a5210d4df97f3a89
Denis Tereschenko <terdenan@gmail.com>
2017-11-14 21:05:55 +0300

    update gitignore

--- homework04/config.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "DOMAIN": "https://api.vk.com/method",
-    "ACCESS_TOKEN": "ee49e1c2501c89069ca7e61d66d481e2a56\
-83db047e61ca6c74fd6ed40286960db4a670f2a70388c9b11b",
-    "client_id": "6232328"
-}
